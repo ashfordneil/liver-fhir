4704624f81e7f60e4deb2b916913c401bfca7fb8
Neil Ashford <ashfordneil0@gmail.com>
2019-10-03 21:50:55 +1000

    updated storybook and loki

--- src/components/Person/Person.stories.tsx
+++ src/components/Person/Person.stories.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withRedux } from '../../../.storybook/decorators';
 
 import Person from './Person';
 
 storiesOf('Person', module)
+  .addDecorator(withRedux({}, []))
   .add('render', () => <Person />);
 
