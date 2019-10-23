58ed06d9aed28f02aaf3e6a5257aa286c626197c
Neil Ashford <ashfordneil0@gmail.com>
2019-10-23 13:07:23 +1000

    made the examination options look like they want to be clicked

--- src/components/ExaminationOption/ExaminationOption.tsx
+++ src/components/ExaminationOption/ExaminationOption.tsx
@@ -37,7 +37,7 @@ const ExaminationOption: React.FC<ExaminationOptionProps> = (props) => {
             meta: undefined,
             request: {method: "POST"}
           })).map(o => client.create(o)));
-        });
+        }).catch(console.warn);
         const event = SelectMethod(props.method);
         dispatch(event);
       }}
